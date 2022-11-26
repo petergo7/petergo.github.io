0b9ad5e3e2668c39372eec4cbe11acece03101ea
petergo7 <110782072+petergo7@users.noreply.github.com>
2022-11-27 04:26:45 +0900

    PU31

--- JavaScript/jsMain.js
+++ JavaScript/jsMain.js
@@ -33,35 +33,12 @@ function dismissHappy() {
     }  
 }
 
-
-/*
-function dismissLog() {
-    var log = document.getElementById('changeLog');
-    var logButton = document.getElementById('changeLogButton');
-
-    var displaySetting = log.style.display;
-
-    if (displaySetting == "block") {
-        log.style.display = "none"
-        logButton.innerHTML = "Show"
-    }
-    else {
-        log.style.display = "block"
-        logButton.innerHTML = "Dismiss"
-    }
-
-}
-*/
-
 const d = new Date();
-let hours = d.getHours();
+var hours = d.getHours();
 
-if (hours < 5) {
-    document.getElementById("header").innerHTML = "Good Morning early bird, and Welcome Back!"
-} else if (hours < 12) {
-    document.getElementById("header").innerHTML = "Good Morning, and Welcome Back!"
-} else if (hours < 23) {
-    document.getElementById("header").innerHTML = "Good Afternoon, and Welcome Back!"
-} else {
-    document.getElementById("header").innerHTML = "Good Night, and Welcome Back!"
+function confirmExit() {
+    var confirmResultExit = confirm("You are leaving this website to go to a secure and safe website. However, other user-generated content is not managed by Peter Go. Would you still like to initiate a redirect?")
+    if (confirmResultExit) {
+        window.location.href = "https://github.com/petergo7/petergo.github.io";
+    }
 }
